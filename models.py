1c39ba98f2f4bec52e663779e9dadbe9bbd9a1f4
mdwecht <mdwecht@gmail.com>
2021-09-19 22:31:30 -0400

    add more Job default field values

--- models.py
+++ models.py
@@ -5,9 +5,9 @@ class Job(models.Model):
     '''
     DSAE Fields Common to all Jobs:
     '''
-    name = models.CharField(max_length=40)
-    type = models.CharField(max_length=40)
-    state = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, default='Analytic-02 Job-00')
+    type = models.CharField(max_length=40, default='AN_02')
+    state = models.CharField(max_length=40, default='IDLE')
     period = models.IntegerField(default=3600)
     last_ts = models.DateTimeField(auto_now_add=True)
     created_ts = models.DateTimeField(auto_now_add=True)
@@ -16,7 +16,7 @@ class Job(models.Model):
     '''
     DSAE Fields Specific to 'analytic_02' Job Type:
     '''
-    script = models.CharField(max_length=20)
+    script = models.CharField(max_length=20, default='slickscript.py')
     parm_01 = models.IntegerField(default=10)
     parm_02 = models.IntegerField(default=20)
 
