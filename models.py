9717547bc975d6ab101b53dd2feb165a72ab625e
mdwecht <mdwecht@gmail.com>
2021-09-19 20:47:18 -0400

    replace Param model with Job

--- models.py
+++ models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class Params(models.Model):
-    name = models.CharField(max_length=20)
-    var_01 = models.IntegerField(default=0)
-    var_02 = models.IntegerField(default=0)
-    var_03 = models.IntegerField(default=0)
+
+class Job(models.Model):
+    '''
+    DSAE Fields Common to all Jobs:
+    '''
+    name = models.CharField(max_length=40)
+    type = models.CharField(max_length=40)
+    state = models.CharField(max_length=40)
+    period = models.IntegerField(default=3600)
+    last_ts = models.DateTimeField(auto_now_add=True)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    '''
+    DSAE Fields Specific to 'analytic_02' Job Type:
+    '''
+    script = models.CharField(max_length=20)
+    parm_01 = models.IntegerField(default=10)
+    parm_02 = models.IntegerField(default=20)
 
     def __str__(self):
         return self.name
